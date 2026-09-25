e25cde48e1a50ce6514896b99d7aa9de7784fc3d
Matheus Costa <mplc@cesar.school>
2026-09-25 18:53:36 -0300

    Adiciona páginas Home, Equipe e Fale Conosco

--- tarefas/models.py
+++ tarefas/models.py
@@ -20,3 +20,15 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+class Sugestao(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20, blank=True)
+    assunto = models.CharField(max_length=200)
+    mensagem = models.TextField()
+    data_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.assunto}'
